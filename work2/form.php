add61a2587d5b61ddb27c8594e9060ec1ffd6785
石川晴香 <haruka.ishikawa@avalon.co.jp>
2018-06-21 15:51:05 +0900

    課題2
    MySQL接続(insert, select)

---
+++ work2/form.php
@@ -0,0 +1,38 @@
+<?php
+    $dbh = new PDO('mysql:host=localhost; dbname=g031o008', 'test', 'SQsUxvXnGOhPur0J');
+    if($dbh == null){
+    	print_r('接続失敗').PHP_EOL;
+    }else{
+        //データが入力されているかどうかチェックする
+        if(array_key_exists('userId',$_POST) OR array_key_exists('password', $_POST))
+        {
+            if($_POST['userId'] == ''){
+                echo "ユーザIDを入力してください";
+            }
+            elseif($_POST['password']==''){
+                echo "パスワードを入力してください";
+            }
+            else{
+                $stmt = $dbh -> prepare("INSERT INTO user (userId, password) VALUES (:userId, :password)");
+                $stmt->bindParam(':userId', $_POST['userId'], PDO::PARAM_STR);
+                $stmt->bindParam(':password', $_POST['password'], PDO::PARAM_STR);
+                $stmt->execute();
+            }
+        }
+    }
+?>
+<!DOCTYPE html>
+<head>
+    <title>ログインフォーム</title>
+</head>
+<body>
+<form method="post">
+    <input type="text" name="userId" placeholder="ユーザID">
+    <input type="password" name="password" placeholder="パスワード">
+    <input type="submit" value="登録する">
+</form>
+
+<form action="record.php">
+    <input type="submit" value="一覧画面を表示する">
+</form>
+</body>
