e086af8aa78a1ee2a0464702cdaf6aadc7f3afdc
石川晴香 <haruka.ishikawa@avalon.co.jp>
2018-06-27 22:02:28 +0900

    ログイン処理
    ログイン処理を作成しました

--- michinoeki/views/login.php
+++ michinoeki/views/login.php
@@ -1,3 +1,49 @@
+<?php
+// セッション開始
+session_start();
+
+// エラーメッセージの初期化
+$errorMessage = "";
+$errorFlag = false;
+
+// ログインボタンが押された場合
+if (isset($_POST["login"])) {
+    // 1. ユーザIDの入力チェック
+    if (empty($_POST["userId"])) {  // emptyは値が空のとき
+        $errorMessage = 'ユーザーIDが未入力です。';
+    } else if (empty($_POST["password"])) {
+        $errorMessage = 'パスワードが未入力です。';
+    }
+
+    if (!empty($_POST["userId"]) && !empty($_POST["password"])) {
+
+        // 3. エラー処理
+        try {
+            $dbh = new PDO('mysql:host=153.126.145.118; dbname=g031o008', 'g031o008', 'g031o008');
+
+            $stmt = $dbh->prepare('SELECT * FROM user WHERE userId = :userId AND password = :password');
+            $stmt->bindParam(':userId', $_POST['userId'], PDO::PARAM_STR);
+            $stmt->bindParam(':password', $_POST['password'], PDO::PARAM_STR);
+            $stmt->execute();
+
+            if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {    //入力したユーザIDとパスワードに一致するデータがあれば
+                session_regenerate_id(true);    //セッション置き換え
+                $_SESSION["userId"] = $row['userId'];   //ユーザIDをセッションに保存
+                header("Location: myPage.php");  // マイページ画面へ遷移
+                exit();  // 処理終了
+            } else {
+                // 該当データなし
+                $errorFlag = true;
+                $errorMessage = 'ユーザーIDあるいはパスワードに誤りがあります。';
+            }
+        } catch (PDOException $e) {
+            $errorFlag = true;
+            $errorMessage = 'データベースエラー';
+        }
+    }
+}
+?>
+
 <!doctype html>
 <html>
   <head>
@@ -30,8 +76,10 @@
           <main class="mdl-layout__content">
               <div class="form-wrapper">
                   <h1 class="sign-title">ログイン</h1>
-                  <p>ログインエラー</p>
-                  <form>
+                  <?php if($errorFlag) {
+                    echo $errorMessage;
+                  } ?>
+                  <form name="loginForm" method="post">
                     <div class="form-item">
                       <label for="userId"></label>
                       <input type="userId" name="userId" required="required" placeholder="ユーザID"></input>
@@ -41,7 +89,7 @@
                       <input type="password" name="password" required="required" placeholder="パスワード"></input>
                     </div>
                     <div class="button-panel">
-                      <input type="submit" class="sign-button" title="ログイン" value="ログイン"></input>
+                      <input type="submit" name="login" class="sign-button" title="ログイン" value="ログイン"></input>
                     </div>
                   </form>
                   <div class="form-footer">
