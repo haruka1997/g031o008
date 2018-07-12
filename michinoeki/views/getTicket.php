5cfd3a17b9ba4a594edc4303b48c9688b4fde11c
石川晴香 <haruka.ishikawa@avalon.co.jp>
2018-07-12 17:58:32 +0900

    7/10~7/15分の進捗

---
+++ michinoeki/views/getTicket.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>切符管理</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+        <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="./../css/main.css">
+        <link rel="stylesheet" href="./../css/myPage.css">
+        <link rel="stylesheet" href="./../css/modal.css">
+
+    </head>
+    <body>
+        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+            <header class="mdl-layout__header">
+                <div class="mdl-layout__header-row" >
+                    <span class="mdl-layout-title"><i class="fas fa-home"></i>    道の駅情報システム</span>
+                    <div class="mdl-layout-spacer"></div>
+                    <nav class="mdl-navigation mdl-layout--large-screen-only">
+                        <div class="logout-button">
+                            <p><a href="./login.php">ログアウト</a></p>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+            <div class="mdl-layout__drawer">
+                <span class="mdl-layout-title">メニュー</span>
+                <nav class="mdl-navigation">
+                    <a class="mdl-navigation__link" href="./myPage.php"><i class="fas fa-home"></i>マイページ</a>
+                    <a class="mdl-navigation__link" href="./search.php"><i class="fas fa-search"></i>道の駅情報</a>
+                    <a class="mdl-navigation__link" href="./visitStation.php"><i class="fas fa-map-marker-alt"></i>訪問駅管理</a>
+                    <a class="mdl-navigation__link" href="./getTicket.php"><i class="fas fa-ticket-alt"></i>切符管理</a>
+                </nav>
+            </div>
+            <main class="mdl-layout__content">
+                <div class="page-content">
+                切符管理ページ(製作中...)
+                </div>
+            </main>
+        </div>
+    </body>
+</html>
--- michinoeki/views/myPage.php
+++ michinoeki/views/myPage.php
@@ -2,20 +2,18 @@
 session_start();
 
 $user = []; //ユーザの情報
+$message = []; //メッセージの情報
 $errorFlag = false; //エラーフラグ
 $errorMessage = ''; //エラーメッセージ
+$select_message = [];   //選択したメッセージ
 
-/**
- * 変更点(7/7): sessionにuserIdが格納されているかチェック
- * されていなければログイン画面に遷移する
- */
 if(!isset($_SESSION['userId'])){
     header('Location: login.php');
 }
 
 //ユーザの情報取得処理
 try {
-    $dbh = new PDO('mysql:host=153.126.145.118; dbname=g031o008', 'g031o008', 'g031o008');
+    $dbh = new PDO('mysql:host=153.126.145.118; dbname=g031o008; charset=utf8;', 'g031o008', 'g031o008');
 
     $stmt = $dbh->prepare('SELECT userName, userPlace, visitStationNum, getTicketNum, introduce, icon FROM user WHERE userId = :userId');    //ユーザIDに合致するユーザ情報の取得
     $stmt->bindParam(':userId', $_SESSION['userId'], PDO::PARAM_STR);
@@ -24,10 +22,6 @@ try {
     if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {    //ユーザ情報がDBにあれば
         $user = $row;
         
-        /**
-         * 変更点(7/7): アイコン画像が登録されているかチェック
-         * されていなければデフォルト画像をセット
-         */
         if($user['icon'] === ""){
             $user['icon'] = base64_encode(file_get_contents('./../img/default.png'));
         }
@@ -42,21 +36,54 @@ try {
     $errorMessage = 'データベースエラー';
 }
 
+//受信メッセージの情報取得処理
+try {
+    $dbh = new PDO('mysql:host=153.126.145.118; dbname=g031o008; charset=utf8;', 'g031o008', 'g031o008');
+
+    $stmt = $dbh->prepare('SELECT * FROM message WHERE toUserId = :toUserId');    //ユーザIDに合致するメッセージ情報の取得
+    $stmt->bindParam(':toUserId', $_SESSION['userId'], PDO::PARAM_STR);
+    $stmt->execute();
+
+    if ($row = $stmt->fetchAll(PDO::FETCH_ASSOC)) {    //ユーザ情報がDBにあれば
+        $message = $row;
+        
+        for($i=0; $i<count($message); $i++){  
+            // 送信ユーザ情報の取得
+            $stmt = $dbh->prepare('SELECT userId, userName, icon FROM user WHERE userId = :userId');    //ユーザIDに合致するユーザ情報の取得
+            $stmt->bindParam(':userId', $message[$i]['fromUserId'], PDO::PARAM_STR);
+            $stmt->execute();
+
+            // 送信ユーザのユーザ名とアイコン情報を格納
+            if($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                $message[$i]['fromUserName'] = $row['userName'];
+                if($row['icon'] === ""){
+                    $message[$i]['icon'] = base64_encode(file_get_contents('./../img/default.png'));
+                }else{
+                    $message[$i]['icon'] = $row['icon'];
+                }
+            }
+        }
+    } else {
+        // 該当データなし
+        // $errorFlag = true;
+        // $errorMessage = 'ユーザ情報取得エラーです'; 
+    }
+} catch (PDOException $e) {
+    $errorFlag = true;
+    $errorMessage = 'データベースエラー';
+}
+
 //ユーザ情報の更新
 if (isset($_POST["regist"])) {
 
     try {
-        $dbh = new PDO('mysql:host=153.126.145.118; dbname=g031o008', 'g031o008', 'g031o008');
+        $dbh = new PDO('mysql:host=153.126.145.118; dbname=g031o008; charset=utf8;', 'g031o008', 'g031o008');
     
         $stmt = $dbh->prepare('UPDATE user SET userName = :userName, userPlace = :userPlace, introduce = :introduce, icon = :icon WHERE userId = :userId');    //ユーザIDに合致するユーザ情報の更新
         $stmt->bindParam(':userName',  $_POST['userName'], PDO::PARAM_STR);  //ユーザ名
         $stmt->bindParam(':userPlace', $_POST['userPlace'], PDO::PARAM_STR);    //居住地
         $stmt->bindParam(':introduce', $_POST['introduce'], PDO::PARAM_STR);    //自己紹介
         
-        /**
-         * 変更点(7/7): アイコン画像を指定しなかった場合に以前の画像が消えてしまうバグ修正
-         * アイコン指定しなかった場合は以前の画像をセット
-         */
         //アイコン指定可否判定
         if($_FILES['icon']['tmp_name'] !== ''){
             $icon = base64_encode(file_get_contents($_FILES['icon']['tmp_name']));  //icon画像base64化
@@ -78,14 +105,13 @@ if (isset($_POST["regist"])) {
         $errorFlag = true;
         $errorMessage = 'データベースエラー';
     }
-
 }
-
 ?>
+
 <!doctype html>
 <html>
     <head>
-        <meta charset="utf-8"/>
+        <meta http-equiv="content-type" charset="utf-8"/>
         <title>マイページ</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
@@ -95,6 +121,7 @@ if (isset($_POST["regist"])) {
         <link rel="stylesheet" href="./../css/main.css">
         <link rel="stylesheet" href="./../css/myPage.css">
         <link rel="stylesheet" href="./../css/modal.css">
+
     </head>
     <body>
         <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
@@ -110,12 +137,12 @@ if (isset($_POST["regist"])) {
                 </div>
             </header>
             <div class="mdl-layout__drawer">
-                <span class="mdl-layout-title">Title</span>
+                <span class="mdl-layout-title">メニュー</span>
                 <nav class="mdl-navigation">
-                    <a class="mdl-navigation__link" href="">Link</a>
-                    <a class="mdl-navigation__link" href="">Link</a>
-                    <a class="mdl-navigation__link" href="">Link</a>
-                    <a class="mdl-navigation__link" href="">Link</a>
+                    <a class="mdl-navigation__link" href="./myPage.php"><i class="fas fa-home"></i>マイページ</a>
+                    <a class="mdl-navigation__link" href="./search.php"><i class="fas fa-search"></i>道の駅情報</a>
+                    <a class="mdl-navigation__link" href="./visitStation.php"><i class="fas fa-map-marker-alt"></i>訪問駅管理</a>
+                    <a class="mdl-navigation__link" href="./getTicket.php"><i class="fas fa-ticket-alt"></i>切符管理</a>
                 </nav>
             </div>
             <main class="mdl-layout__content">
@@ -130,7 +157,6 @@ if (isset($_POST["regist"])) {
                         <?= $errorMessage ?>
                         <!-- プロフィール情報 -->
                         <div class="profile-table">
-                            <!-- 変更点(7/7): ユーザ情報の表示についてXSS対策追加 -->
                             <table class="table">
                                 <tbody>
                                 <tr>
@@ -149,7 +175,6 @@ if (isset($_POST["regist"])) {
                                     <th>入手切符数</th>
                                     <td><?= htmlspecialchars($user['getTicketNum'], ENT_QUOTES, "UTF-8") ?></td>
                                 </tr>
-                                <!-- 変更点(7/7): 自己紹介文の改行を反映できるように修正 -->
                                 <tr>
                                     <th>自己紹介</th>
                                     <td style="height:200px;"><pre><?= htmlspecialchars($user['introduce'], ENT_QUOTES, "UTF-8") ?></pre></td>
@@ -160,17 +185,17 @@ if (isset($_POST["regist"])) {
                     </div>
                     <!-- 編集ボタン -->
                     <div class="edit-button">
-                        <label for="modal-trigger" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">編集</label>
+                        <label for="edit-modal-trigger" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">編集</label>
                     </div>
                 </div>
 
                 <!-- 編集モーダル -->
                 <div class="modal">
-                    <input id="modal-trigger" class="checkbox" type="checkbox">
+                    <input id="edit-modal-trigger" class="checkbox" type="checkbox">
                     <div class="modal-overlay">
-                        <label for="modal-trigger" class="o-close"></label>
+                        <label for="edit-modal-trigger" class="o-close"></label>
                         <div class="modal-wrap">
-                            <label for="modal-trigger" class="close">×</label>
+                            <label for="edit-modal-trigger" class="close">×</label>
                             <!-- モーダルヘッダ -->
                             <div class="modal-header">
                                 マイページ編集
@@ -193,7 +218,6 @@ if (isset($_POST["regist"])) {
                                             <tr>
                                                 <th>居住地</th>
                                                 <td>
-                                                <!-- 変更点(7/7): 初期値に登録されたデータを反映 -->
                                                 <select name="userPlace">
                                                     <option value="県北" <?= $user['userPlace'] != '県北' ?: 'selected' ?>>県北</option>
                                                     <option value="沿岸" <?= $user['userPlace'] != '沿岸' ?: 'selected' ?>>沿岸</option>
@@ -226,18 +250,51 @@ if (isset($_POST["regist"])) {
                     </div>
                     <!-- メッセージ一覧 -->
                     <div class="message-list">
-                        <p>テストメッセージ</p>
-                        <p>テストです</p>
-                        <p>テストメッセージ</p>
-                        <p>テストです</p>
-                        <p>テストメッセージ</p>
-                        <p>テストです</p>
-                        <p>テストメッセージ</p>
-                        <p>テストです</p>
-                        <p>テストメッセージ</p>
-                        <p>テストです</p>
+                        <?php foreach($message as $key => $value){?>
+                            <form type="post">
+                                 <p><label for="send-mail-modal-trigger<?= $key ?>"><?= $message[$key]['title'] ?></input></p>
+                            </form>
+                        <?php } ?>
                     </div>
                 </div>
+
+                <!-- 受信メッセージモーダル -->
+                <?php foreach($message as $key => $value){?>
+                    <div class="modal">
+                        <input id="send-mail-modal-trigger<?= $key ?>" class="checkbox" type="checkbox">
+                        <div class="modal-overlay">
+                            <label for="send-mail-modal-trigger<?= $key ?>" class="o-close"></label>
+                            <div class="modal-wrap">
+                                <label for="send-mail-modal-trigger<?= $key ?>" class="close">×</label>
+                                <!-- モーダルヘッダ(タイトル) -->
+                                <div class="title-header">
+                                    <p><?= $message[$key]['title'] ?></p>
+                                </div>
+                                <div class="title-sub-header">
+                                    <!-- 送信ユーザ -->
+                                    <div class="send-user">
+                                        <img border="0" src="data:image/png;base64, <?= $message[$key]['icon'] ?>" width="40">
+                                        <label><?= $message[$key]['fromUserName'] ?></label>
+                                    </div>
+                                    <!-- 送信日時 -->
+                                    <div class="send-date">
+                                        <label><?= $message[$key]['created'] ?></label>
+                                    </div>
+                                </div>
+                                <!-- メール本文 -->
+                                <div class="body">
+                                    <div class="mail-body">
+                                    <?= $message[$key]['body'] ?>
+                                    </div>
+                                </div>
+                                <!-- 送信ボタン -->
+                                <div class="regist-button">
+                                    <button type="submit" name="regist" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">返信</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php }  ?>
             </main>
         </div>
     </body>
